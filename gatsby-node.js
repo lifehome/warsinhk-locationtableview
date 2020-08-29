c9f2edf5dbc24c2a36ec5bfaab3a1ac8623fd229
Ivan Ip <m@lifeho.me>
2020-08-30 03:43:52 +0800

    add graphql node Codes copied from https://github.com/vote4hk/warsinhk/blob/b1466c03f9691ba59728d44ab710d9164d952adb/gatsby-node.js#L332

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,72 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+const fetch = require("node-fetch")
+const csv2json = require("csvtojson")
+
+const PUBLISHED_SPREADSHEET_WARS_CASES_LOCATION_URL =
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vT6aoKk3iHmotqb5_iHggKc_3uAA901xVzwsllmNoOpGgRZ8VAA3TSxK6XreKzg_AUQXIkVX5rqb0Mo/pub?gid=0"
+
+exports.sourceNodes = async props => {
+  createPublishedGoogleSpreadsheetNode(
+    props,
+    PUBLISHED_SPREADSHEET_WARS_CASES_LOCATION_URL,
+    "WarsCaseLocation",
+    {
+      skipFirstLine: true,
+      subtype: "default",
+    }
+  )
+}
+
+const createPublishedGoogleSpreadsheetNode = async (
+    { actions: { createNode, createTypes }, createNodeId, createContentDigest },
+    publishedURL,
+    type,
+    { skipFirstLine = false, alwaysEnabled = false, subtype = null }
+  ) => {
+    // All table has first row reserved
+    const result = await fetch(
+      `${publishedURL}&single=true&output=csv&headers=0${
+        skipFirstLine ? "&range=A2:ZZ" : ""
+      }&q=${Math.floor(new Date().getTime(), 1000)}`
+    )
+    const data = await result.text()
+    const records = await csv2json().fromString(data)
+    const filteredRecords = records.filter(
+      r => alwaysEnabled || r.enabled === "Y"
+    )
+  
+    if (filteredRecords.length > 0) {
+      filteredRecords.forEach((p, i) => {
+        // create node for build time data example in the docs
+        const meta = {
+          // required fields
+          id: createNodeId(
+            `${type.toLowerCase()}${subtype ? `-${subtype}` : ""}-${i}`
+          ),
+          parent: null,
+          children: [],
+          internal: {
+            type,
+            contentDigest: createContentDigest(p),
+          },
+        }
+        const node = { ...p, subtype, ...meta }
+        createNode(node)
+      })
+    } else if (records.length > 0) {
+      // So if filtered rows is empty,
+      // we manually create the type here.
+  
+      // TODO: handle not even a single row ...
+      const fields = Object.keys(records[0])
+      const fieldString = fields.map(field => `${field}: String`).join("\n")
+      const typeTemplate = `
+        type ${type} implements Node {
+          ${fieldString}
+        }
+      `
+      createTypes(typeTemplate)
+    }
+  }
