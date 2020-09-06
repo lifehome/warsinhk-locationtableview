8cad3ea4fdb312f433251f00304cbfd2d1f4aa35
Ivan Ip <m@lifeho.me>
2020-09-06 15:51:27 +0800

    change theme color to match wars main site

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `rgb(80, 80, 150)`,
       marginBottom: `1.45rem`,
     }}
   >
